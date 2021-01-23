a853b3dcf3c75d10e2644ea2defb1f360f353e5b
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-01-23 19:51:05 +0200

    Add dependency injection to inializer

---
+++ spec/note_spec.rb
@@ -0,0 +1,17 @@
+require 'simplecov'
+SimpleCov.start
+require 'note'
+
+describe Note do
+  
+  note_formatter = NoteFormatter.new
+  subject { described_class.new('Today\'s things', 'To mop, cook, play', note_formatter) }
+  
+  it 'has a tile when initialized' do
+    expect(subject.title).to eq 'Today\'s things'
+  end
+
+  it 'has a body when initialized' do
+    expect(subject.body).to eq 'To mop, cook, play'
+  end
+end

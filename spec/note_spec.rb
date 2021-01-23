2ac7211a69fc67f5fc9da43f3a81f0cffd6fa9ad
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-01-23 19:55:57 +0200

    It displays the note

--- spec/note_spec.rb
+++ spec/note_spec.rb
@@ -14,4 +14,10 @@ describe Note do
   it 'has a body when initialized' do
     expect(subject.body).to eq 'To mop, cook, play'
   end
+
+  describe '#display' do
+    it 'formats the note' do
+      expect(subject.display).to eq "Title: #{subject.title}""\n""#{subject.body}"
+    end
+  end
 end
